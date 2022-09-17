64df6467d6735a261ebf991b1542d0f364369e03
Alexey Kuzmenko <alexey.kuzmenko1101@gmail.com>
2022-09-17 10:40:19 +0300

    - added api config - added api service

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,11 @@
 // * styles
 import '../scss/style.scss'
+import api from "./service/apiService";
+
+api.countries()
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+api.cities()
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
