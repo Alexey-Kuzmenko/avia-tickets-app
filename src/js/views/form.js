ddb77cd3899687141209bbec8c9d7f8ecc8b600a
Alexey Kuzmenko <alexey.kuzmenko1101@gmail.com>
2022-10-09 05:42:55 +0300

    Change checkInputData method
    Remove unnecessary code and change logic of checkInputData method.

--- src/js/views/form.js
+++ src/js/views/form.js
@@ -21,27 +21,23 @@ class FormUI {
     }
 
     get originValue() {
-        // return this.origin.value
         return FormUI.checkInputData(this.origin.value)
     }
 
     get destinationValue() {
-        // return this.destination.value
         return FormUI.checkInputData(this.destination.value)
     }
 
     get departDateValue() {
-        // return this.departDate.value
         return FormUI.checkInputData(this.departDate.value)
     }
 
     get returnDateValue() {
-        // return this.returnDate.value
         return FormUI.checkInputData(this.returnDate.value)
     }
 
     static checkInputData(inputValue) {
-        if (inputValue === '') {
+        if (!inputValue.length) {
             alert('Please fill in all inputs')
             return
         } else {
