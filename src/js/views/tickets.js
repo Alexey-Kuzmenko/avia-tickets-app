b3aa458695aed243eca36d1e393edabfcef074e9
Alexey Kuzmenko <alexey.kuzmenko1101@gmail.com>
2022-10-25 15:55:27 +0300

    Change tickets UI design
    Now tickets have an 'add to favourite' button.

--- src/js/views/tickets.js
+++ src/js/views/tickets.js
@@ -45,35 +45,38 @@ class TicketsUI {
 
     static ticketTemplate(ticket, currency) {
         return `
-            <div class="card tickets-card" data-ticket-id="${ticket.id}">
-                        <img src="${ticket.airline_logo}" class="card-img-top" alt="Airline logo">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">${ticket.airline_name}</h5>
-                            <hr>
-                            <div class="card-text text-center my-3">
-                                <span class="fs-5 fw-bold">Origin: ${ticket.origin_name}</span>
-                                <span class="fs-5 fw-bold">Destination: ${ticket.destination_name}</span>
-                            </div>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item px-0"><i class="bi bi-calendar2-event"></i> 
-                                    Depart date: ${ticket.departure_at}
-                                </li>
-                                <li class="list-group-item px-0"><i class="bi bi-calendar2-event"></i> 
-                                    Return date: ${ticket.return_at}
-                                </li>
-                                <li class="list-group-item px-0"><i class="bi bi-airplane"></i> 
-                                    Flight number: ${ticket.flight_number}
-                                </li>
-                                <li class="list-group-item px-0"><i class="bi bi-arrow-down-up"></i> Transfers: 
-                                    ${ticket.transfers}
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="card-footer">
-                            <p class="fs-5 fw-bold mb-0">Price: ${currency}${ticket.price}</p>
-                            <a href="#" class="btn btn-primary text-uppercase">Buy</a>
-                        </div>
+        <div class="card tickets-card" data-ticket-id="${ticket.id}">
+            <img src="${ticket.airline_logo}" class="card-img-top" alt="Airline logo">
+            <div class="card-body">
+                <h5 class="card-title text-center">${ticket.airline_name}</h5>
+                <hr>
+                <div class="card-text text-center my-3">
+                    <span class="fs-5 fw-bold">Origin: ${ticket.origin_name}</span>
+                    <span class="fs-5 fw-bold">Destination: ${ticket.destination_name}</span>
                 </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item px-0"><i class="bi bi-calendar2-event"></i>
+                        Depart date: ${ticket.departure_at}
+                    </li>
+                    <li class="list-group-item px-0"><i class="bi bi-calendar2-event"></i>
+                        Return date: ${ticket.return_at}
+                    </li>
+                    <li class="list-group-item px-0"><i class="bi bi-airplane"></i>
+                        Flight number: ${ticket.flight_number}
+                    </li>
+                    <li class="list-group-item px-0"><i class="bi bi-arrow-down-up"></i> Transfers:
+                        ${ticket.transfers}
+                    </li>
+                </ul>
+            </div>
+            <div class="card-footer">
+                <p class="fs-5 fw-bold mb-0">Price: ${currency}${ticket.price}</p>
+                <div class="card-footer-controls d-flex justify-content-between">
+                    <a href="#" class="btn btn-primary text-uppercase">Buy</a>
+                    <a class="btn btn-light add-to-favorites-btn"><i class="bi bi-heart"></i></a>
+                </div>
+            </div>
+        </div>
         `
     }
 }
